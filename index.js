3a9d03948143cf5088ff0a8d3de1edb9f83ed7fc
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2022-03-15 16:10:40 -0600

    commit tree d7a368d68e481b5d8e777b7147b21b7208a355b9

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3/num4;
+
+const max = Math.max(2, 4, 6, 8, 20);
